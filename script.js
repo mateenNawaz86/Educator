8f035de0a459dbc06d052effda69529e730cdd7b
Mateen Mirani <mateenmirani125@gmail.com>
2022-09-04 07:50:24 +0500

    FAQ'S section

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ window.addEventListener("scroll", () => {
     .querySelector("nav")
     .classList.toggle("window-scroll", window.scrollY > 0);
 });
+
+// FAQ functionality
+const faqs = document.querySelectorAll(".faq");
+
+console.log(faqs);
+
+faqs.forEach((item) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("answer");
+
+    // change the icon
+    let faqIcon = document.querySelector(".faqs__icon i");
+
+    if (faqIcon.className == "uil uil-plus") {
+      faqIcon.className = "uil uil-minus";
+    } else {
+      faqIcon.className = "uil uil-plus";
+    }
+  });
+});
