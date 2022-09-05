cced9b2ce3cdcc6d98ce92f59428e0e66ff295d8
Mateen Mirani <mateenmirani125@gmail.com>
2022-09-05 16:54:58 +0500

    Footer, responsiveness

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ window.addEventListener("scroll", () => {
 // FAQ functionality
 const faqs = document.querySelectorAll(".faq");
 
-console.log(faqs);
+// console.log(faqs);
 
 faqs.forEach((item) => {
   item.addEventListener("click", () => {
     item.classList.toggle("answer");
 
     // change the icon
-    let faqIcon = document.querySelector(".faqs__icon i");
+    let faqIcon = item.querySelector(".faqs__icon i");
 
     if (faqIcon.className == "uil uil-plus") {
       faqIcon.className = "uil uil-minus";
@@ -22,4 +22,21 @@ faqs.forEach((item) => {
       faqIcon.className = "uil uil-plus";
     }
   });
+});
+
+// Navbar show in Mobile devices
+const hamburgerBtn = document.getElementById("open-menu-btn");
+
+const menu = document.querySelector(".nav__menu");
+
+hamburgerBtn.addEventListener("click", () => {
+  menu.classList.toggle("show");
+
+  let toggleBtn = document.querySelector("button i");
+
+  if (toggleBtn.className == "uil uil-bars") {
+    toggleBtn.className = "uil uil-multiply";
+  } else {
+    toggleBtn.className = "uil uil-bars";
+  }
 });
